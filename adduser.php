f27a62fc86d4dcece3321403660802beedef9fb4
Shubham <shubhamthapliyal1825@outlook.com>
2021-03-12 17:34:41 +0530

    add some changes in crud.js,delete.php,adduser.php

--- adduser.php
+++ adduser.php
@@ -3,20 +3,19 @@
 include("connection.php");
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-
-
-
     $name = $_POST['name'];
     $email = $_POST['emailid'];
     $password = $_POST['password'];
     $number = $_POST['phone'];
-
     $sql =  "INSERT INTO ajax(name,email,password,phone) values('$name', '$email','$password','$number')";
-
-
-
     $result = mysqli_query($conn, $sql);
-    if ($result) {
-        echo "A NEW ENTRY IS ADDED";
+    $json = array();
+    if($result){
+        $json = array('status'=>true, 'name'=>$name, 'email'=>$email,'phone'=>$number);
+    }else{
+        $json = array('status'=>false);
     }
+   
+    echo json_encode($json);
+    
 }
